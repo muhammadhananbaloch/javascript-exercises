f44c3b07a8f6cedb56b7b7ea4639f2bd3decd792
Muhammad Hanan Baloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2023-09-07 22:31:00 +0500

    Test 1 passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let empty = ""
+    for(let i = 0; i < number; i++){
+        empty += string;
+    }
+    return empty;
 };
 
 // Do not edit below this line

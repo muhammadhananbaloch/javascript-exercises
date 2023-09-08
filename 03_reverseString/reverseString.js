4b96f193d834fb9315e06eeadfa3e19bcaa2a644
Muhammad Hanan Baloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2023-09-08 13:33:37 +0500

    Reduce code.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,5 @@
 const reverseString = function(string) {
-    let splitString = string.split("");
-    let reverseArray = splitString.reverse();
-    let joinArray = reverseArray.join("");
-    return joinArray;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line

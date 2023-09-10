3bec290ed85b9610ccfe3cfbf2a1c597f4cb06a8
Muhammad Hanan Baloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2023-09-10 13:40:59 +0500

    Organise code.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,12 @@
 const removeFromArray = function(array, ...elementToRemove) {
-
+    
     for(const element of elementToRemove){
         const index = array.indexOf(element);
         if(index !== -1){
            array.splice(index, 1);
-            }
+        }
     }
     return array;      
-    
 };
 // Do not edit below this line
 module.exports = removeFromArray;

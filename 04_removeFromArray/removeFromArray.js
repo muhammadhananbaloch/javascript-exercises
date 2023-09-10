7d899a246b69cd34068ece39be09645fc28451b4
Muhammad Hanan Baloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2023-09-10 13:49:33 +0500

    Add comments for my own understanding when I come back in future.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,10 @@
 const removeFromArray = function(array, ...elementToRemove) {
-    
-    for(const element of elementToRemove){
-        const index = array.indexOf(element);
-        if(index !== -1){
-           array.splice(index, 1);
-        }
+
+    for(const element of elementToRemove){    //since the elementToRemove argument accepts optional parameter as an array. the for ...of 
+        const index = array.indexOf(element); //loop stores each passed parameter to remove one by one and checks whether the index is -1
+        if(index !== -1){                     //or not. if the condition is true it splices the passed array from the index number of the 
+           array.splice(index, 1);            //element to remove to one and the for of loop continues until all the elementsToRemove are
+        }                                     //removed.After the loops breaks it returns the array after all the elementToRemove are splices. 
     }
     return array;      
 };

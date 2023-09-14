e28512205627a0d222a9b1014e52b9681b390105
Muhammad Hanan Baloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2023-09-14 13:14:47 +0500

    Pass test 3.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,16 @@
 const sumAll = function(num1, num2) {
-    let sum= 0;
-    for(let i=num1; i<=num2; i++){
-        sum +=i;
+    let total= 0;
+    if (num2 < num1){
+        for(let i=num2; i<=num1; i++){
+        total +=i;
     }
-    return sum;
-
+    return total;
+    }else{
+        for(let i=num1; i<=num2; i++){
+        total +=i;
+    }
+    return total;
+        }
 };
 
 // Do not edit below this line
